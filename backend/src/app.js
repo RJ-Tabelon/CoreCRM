2207269f84262122031b47aa547acb3ce42f815e
RJ Tabelon <tabelonrj@gmail.com>
2026-01-02 20:19:18 -0500

    Implement user CRUD

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from '#routes/auth.routes.js';
+import usersRoutes from '#routes/users.routes.js';
 import securityMiddleware from '#middleware/security.middleware.js';
 
 
@@ -43,5 +44,7 @@ app.get('/api', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
+
 
 export default app;
